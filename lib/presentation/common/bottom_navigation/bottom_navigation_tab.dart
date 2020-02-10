3e671ef9f1a397326e1c313ed4e1a6a5b4fcde8c
Edson Bueno <ejr.bueno@gmail.com>
2020-02-09 21:25:34 -0300

    Create HomeScreen widget with Bottom Navigation setup.

---
+++ lib/presentation/common/bottom_navigation/bottom_navigation_tab.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+
+/// Contains the necessary parameters for building a
+/// [BottomNavigationScaffold].
+/// Detailed tutorial: https://edsonbueno.com/2020/01/23/bottom-navigation-in-flutter-mastery-guide/
+class BottomNavigationTab {
+  const BottomNavigationTab({
+    @required this.bottomNavigationBarItem,
+    @required this.navigatorKey,
+    @required this.initialPageBuilder,
+  })  : assert(bottomNavigationBarItem != null),
+        assert(navigatorKey != null),
+        assert(initialPageBuilder != null);
+
+  final BottomNavigationBarItem bottomNavigationBarItem;
+  final GlobalKey<NavigatorState> navigatorKey;
+  final Widget Function() initialPageBuilder;
+}
---
+++ lib/presentation/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:breaking_bapp/presentation/common/bottom_navigation/bottom_navigation_scaffold.dart';
+import 'package:breaking_bapp/presentation/common/bottom_navigation/bottom_navigation_tab.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) => BottomNavigationScaffold(
+        navigationBarItems: [
+          BottomNavigationTab(
+            bottomNavigationBarItem: BottomNavigationBarItem(
+              title: const Text('Characters'),
+              icon: Icon(Icons.people),
+            ),
+            navigatorKey: GlobalKey<NavigatorState>(),
+            initialPageBuilder: () => Container(),
+          ),
+          BottomNavigationTab(
+            bottomNavigationBarItem: BottomNavigationBarItem(
+              title: const Text('Quotes'),
+              icon: Icon(Icons.format_quote),
+            ),
+            navigatorKey: GlobalKey<NavigatorState>(),
+            initialPageBuilder: () => Container(),
+          )
+        ],
+      );
+}
