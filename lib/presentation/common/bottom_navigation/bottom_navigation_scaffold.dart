70eadedd6b4861983310a959dc2a8b2046f72ea1
Edson Bueno <ejr.bueno@gmail.com>
2020-02-11 08:06:51 -0300

    Add cross-fade and lazy init to BottomNavigationScaffold

--- lib/presentation/common/bottom_navigation/bottom_navigation_scaffold.dart
+++ lib/presentation/common/bottom_navigation/bottom_navigation_scaffold.dart
@@ -8,15 +8,16 @@ import 'package:flutter/widgets.dart';
 class BottomNavigationScaffold extends StatefulWidget {
   const BottomNavigationScaffold({
     @required this.navigationBarItems,
-    this.onItemSelected,
+    @required this.onItemSelected,
     Key key,
   })  : assert(navigationBarItems != null),
         super(key: key);
 
   /// List of the tabs to be displayed with their respective navigator's keys
-  /// and initial page builders.
+  /// and initial widget builders.
   final List<BottomNavigationTab> navigationBarItems;
 
+  /// Called when a tab selection occurs.
   final ValueChanged<int> onItemSelected;
 
   @override
@@ -24,49 +25,139 @@ class BottomNavigationScaffold extends StatefulWidget {
       _BottomNavigationScaffoldState();
 }
 
-class _BottomNavigationScaffoldState extends State<BottomNavigationScaffold> {
+class _BottomNavigationScaffoldState extends State<BottomNavigationScaffold>
+    with TickerProviderStateMixin<BottomNavigationScaffold> {
+  final List<_MaterialBottomNavigationTab> materialNavigationBarItems = [];
+  final List<AnimationController> _animationControllers = [];
   int _currentlySelectedIndex = 0;
 
+  /// Controls which tabs should have its content built. This enables us to
+  /// lazy instantiate it.
+  final List<bool> _shouldBuildTab = <bool>[];
+
   @override
-  Widget build(BuildContext context) => WillPopScope(
-        // We're preventing the root navigator from popping and closing the app
-        // when the back button is pressed and the inner navigator can handle
-        // it. That occurs when the inner has more than one page on its stack.
-        // You can comment the onWillPop callback and watch "the bug".
-        onWillPop: () async => !await widget
-            .navigationBarItems[_currentlySelectedIndex]
-            .navigatorKey
-            .currentState
-            .maybePop(),
-        child: Scaffold(
-          // The IndexedStack is what allows us to retain state across tab
-          // switches by keeping our views in the widget tree while only showing
-          // the selected one.
-          body: IndexedStack(
-            index: _currentlySelectedIndex,
-            children: widget.navigationBarItems
-                .map(
-                  (item) => Navigator(
-                    key: item.navigatorKey,
-                    onGenerateRoute: (settings) => MaterialPageRoute(
-                      settings: settings,
-                      builder: (context) => item.initialPageBuilder(context),
-                    ),
-                  ),
-                )
-                .toList(),
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: _currentlySelectedIndex,
-            items: widget.navigationBarItems
-                .map(
-                  (item) => item.bottomNavigationBarItem,
-                )
-                .toList(),
-            onTap: onTabSelected,
-          ),
+  void initState() {
+    _initAnimationControllers();
+    _initMaterialNavigationBarItems();
+
+    _shouldBuildTab.addAll(List<bool>.filled(
+      widget.navigationBarItems.length,
+      false,
+    ));
+
+    super.initState();
+  }
+
+  void _initMaterialNavigationBarItems() {
+    materialNavigationBarItems.addAll(
+      widget.navigationBarItems
+          .map((barItem) => _MaterialBottomNavigationTab(
+                bottomNavigationBarItem: barItem.bottomNavigationBarItem,
+                navigatorKey: barItem.navigatorKey,
+                initialPageBuilder: barItem.initialPageBuilder,
+                subtreeKey: GlobalKey(),
+              ))
+          .toList(),
+    );
+  }
+
+  void _initAnimationControllers() {
+    _animationControllers.addAll(
+      widget.navigationBarItems.map<AnimationController>(
+        (destination) => AnimationController(
+          vsync: this,
+          duration: const Duration(milliseconds: 200),
+        ),
+      ),
+    );
+
+    if (_animationControllers.isNotEmpty) {
+      _animationControllers[0].value = 1.0;
+    }
+  }
+
+  @override
+  void dispose() {
+    _animationControllers.forEach(
+      (controller) => controller.dispose(),
+    );
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        // The Stack is what allows us to retain state across tab
+        // switches by keeping all of our views in the widget tree.
+        body: Stack(
+          fit: StackFit.expand,
+          children: materialNavigationBarItems
+              .map((item) => _buildPageFlow(
+                  context, materialNavigationBarItems.indexOf(item), item))
+              .toList(),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentlySelectedIndex,
+          items: materialNavigationBarItems
+              .map(
+                (item) => item.bottomNavigationBarItem,
+              )
+              .toList(),
+          onTap: onTabSelected,
         ),
       );
+
+  // The best practice here would be to extract this to another Widget,
+  // however, moving it to a separate class would only harm the
+  // readability of our guide.
+  Widget _buildPageFlow(
+    BuildContext context,
+    int tabIndex,
+    _MaterialBottomNavigationTab item,
+  ) {
+    final isCurrentlySelected = tabIndex == _currentlySelectedIndex;
+
+    // We should build the tab content only if it was already built or
+    // if it is currently selected.
+    _shouldBuildTab[tabIndex] =
+        isCurrentlySelected || _shouldBuildTab[tabIndex];
+
+    final Widget view = FadeTransition(
+      opacity: _animationControllers[tabIndex].drive(
+        CurveTween(curve: Curves.fastOutSlowIn),
+      ),
+      child: KeyedSubtree(
+        key: item.subtreeKey,
+        child: _shouldBuildTab[tabIndex]
+            ? Navigator(
+                // The key enables us to access the Navigator's state inside the
+                // onWillPop callback and for emptying its stack when a tab is
+                // re-selected. That is why a GlobalKey is needed instead of
+                // a simpler ValueKey.
+                key: item.navigatorKey,
+                // Since this isn't the purpose of this sample, we're not using
+                // named routes. Because of that, the onGenerateRoute callback
+                // will be called only for the initial route.
+                onGenerateRoute: (settings) => MaterialPageRoute(
+                  settings: settings,
+                  builder: (context) => item.initialPageBuilder(context),
+                ),
+              )
+            : Container(),
+      ),
+    );
+
+    if (tabIndex == _currentlySelectedIndex) {
+      _animationControllers[tabIndex].forward();
+      return view;
+    } else {
+      _animationControllers[tabIndex].reverse();
+      if (_animationControllers[tabIndex].isAnimating) {
+        return IgnorePointer(child: view);
+      }
+      return Offstage(child: view);
+    }
+  }
 
   /// Called when a tab selection occurs.
   void onTabSelected(int newIndex) {
@@ -86,4 +177,25 @@ class _BottomNavigationScaffoldState extends State<BottomNavigationScaffold> {
       widget.onItemSelected(newIndex);
     }
   }
+}
+
+/// Extension class of BottomNavigationTab that adds another GlobalKey to it
+/// in order to use it within the KeyedSubtree widget.
+class _MaterialBottomNavigationTab extends BottomNavigationTab {
+  const _MaterialBottomNavigationTab({
+    @required BottomNavigationBarItem bottomNavigationBarItem,
+    @required GlobalKey<NavigatorState> navigatorKey,
+    @required WidgetBuilder initialPageBuilder,
+    @required this.subtreeKey,
+  })  : assert(bottomNavigationBarItem != null),
+        assert(subtreeKey != null),
+        assert(navigatorKey != null),
+        assert(initialPageBuilder != null),
+        super(
+          bottomNavigationBarItem: bottomNavigationBarItem,
+          navigatorKey: navigatorKey,
+          initialPageBuilder: initialPageBuilder,
+        );
+
+  final GlobalKey subtreeKey;
 }
