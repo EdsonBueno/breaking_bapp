e51866f5466f4038f7f4f30397a320b05480cd50
Edson Bueno <ejr.bueno@gmail.com>
2020-02-13 08:09:53 -0300

    Configure Fluro Router and Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,64 @@
 import 'package:breaking_bapp/presentation/home_screen.dart';
+import 'package:breaking_bapp/presentation/scene/character/detail/character_detail_page.dart';
+import 'package:breaking_bapp/presentation/scene/character/list/character_list_page.dart';
+import 'package:breaking_bapp/presentation/scene/quote/quote_list_page.dart';
+import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<Router>.value(
+          value: Router()
+            ..define(
+              '/characters',
+              transitionType: TransitionType.native,
+              handler: Handler(
+                handlerFunc: (context, _) => CharacterListPage(),
+              ),
+            )
+            ..define(
+              '/characters/:id',
+              transitionType: TransitionType.native,
+              handler: Handler(
+                handlerFunc: (_, params) {
+                  final id = int.parse(params['id'][0]);
+                  return CharacterDetailPage(
+                    id: id,
+                  );
+                },
+              ),
+            )
+            ..define(
+              '/quotes',
+              transitionType: TransitionType.native,
+              handler: Handler(
+                handlerFunc: (context, _) => QuoteListPage(),
+              ),
+            )
+            ..define(
+              '/character',
+              transitionType: TransitionType.nativeModal,
+              handler: Handler(
+                handlerFunc: (_, params) {
+                  final name = params['name'][0];
+                  return CharacterDetailPage(
+                    name: name,
+                  );
+                },
+              ),
+            ),
+        ),
+        ProxyProvider<Router, RouteFactory>(
+          update: (context, router, _) => router.generator,
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -11,5 +68,9 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.green,
         ),
         home: HomeScreen(),
+        onGenerateRoute: Provider.of<RouteFactory>(
+          context,
+          listen: false,
+        ),
       );
 }
